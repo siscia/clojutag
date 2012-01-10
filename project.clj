c5612bb7212a0c9e544744867ca4ada1d0f9fbd3
siscia <simone@mweb.biz>
2012-01-09 21:29:12 -0600

    change evrything

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
 (defproject clojutag "1.0.0-SNAPSHOT"
-  :description "A little utily for music tag in clojure"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+		 [org/jaudiotagger "2.0.3"]]
+  :main clojutag.core)
