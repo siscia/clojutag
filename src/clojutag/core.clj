ad2c870af274b608bb15cfd88b3815f3a4e379ce
siscia <simone@mweb.biz>
2012-01-22 21:56:26 -0600

    try to paralized the process---fail

--- src/clojutag/core.clj
+++ src/clojutag/core.clj
@@ -4,50 +4,54 @@
 	   [org.jaudiotagger.tag Tag FieldKey]
 	   [org.jaudiotagger.audio.real RealTag]
 	   [com.echonest.api.v4 Song EchoNestAPI])
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str])
+  (:use clojure.java.io))
 
 (def api-key "SP3VJBGXDTYFD6IBT")
 
 (def echo (new EchoNestAPI api-key))
 
+(def percorso "/home/simo/Music/a.mp3")
+(def dire "/home/simo/music-to-try")
 
 (defn dizio [a]
   {(. FieldKey ARTIST) (.getArtistName a)
    (. FieldKey ALBUM) (.getReleaseName a)
    (. FieldKey TITLE) (.getTitle a)})
 
-(defn -main [& args]
-  (let [api-key "SP3VJBGXDTYFD6IBT"
-	echo (new EchoNestAPI api-key)
-	f (new java.io.File "C:/Users/mosciatti/Desktop/b.mp3")
-	track (.uploadTrack echo f true)]
-    (do
-       
-    (println (dizio track) track ))))
 
-(defn find-tags [path-to-file]
+(defn write-tag [f info-to-write]
+  (let [audiofileIO (new AudioFileIO)
+        audiofile (.readFile audiofileIO f)
+        tag (.getTag audiofile)
+        newtag (.createDefaultTag audiofile)]
+    (do
+      (doall
+       (map (fn [[fieldkey info]] (.setField newtag fieldkey info)) info-to-write))
+      (.deleteTag audiofileIO audiofile)
+      (.setTag audiofile newtag)
+      (.commit audiofile)
+      (pr info-to-write))))
+
+(defn write-tag-song [f]
+  (let [echo (new EchoNestAPI api-key)
+        track (.uploadTrack echo f true)
+        d (dizio track)]
+    (write-tag f d)))
+
+(defn get-info [path-to-file]
   (let [api-key "SP3VJBGXDTYFD6IBT"
 	echo (new EchoNestAPI api-key)
 	f (new java.io.File path-to-file)
 	track (.uploadTrack echo f true)]
     (dizio track)))
 
+(defn walk-directory [dir]
+  (let [dir (clojure.java.io/file dir)]
+    (file-seq dir)))
 
-
-(defn -main2 [& args]
-  (let [fl (new java.io.File "C:/Users/mosciatti/Desktop/Rihanna Umbrella PARODY Chris Brown - Copy.mp3")
-	inputfile (new AudioFileIO)
-	audiofile (.readFile inputfile fl)
-	tag (.getTag audiofile)
-	artist (.getFirst tag (. FieldKey ARTIST))
-	title (.getFirst tag (. FieldKey TITLE))
-;	newtag (.createDefaultTag audiofile)
-	newtag (new RealTag)]
-    (do
-      (.deleteTag inputfile audiofile)
-      (.setField newtag (. FieldKey ARTIST) (str/upper-case artist))
-      (.setField newtag (. FieldKey TITLE) (str/upper-case title))
-      ;(.setTag audiofile newtag)
-      ;(.commit audiofile)
-      (println title "byyyyyyyyyyyyyyyyyyyyy" artist))))
+(defn -main [& args]
+  (let [s (walk-directory dire)]
+    (doall
+     (pmap write-tag-song (drop 1 s)))))
 
